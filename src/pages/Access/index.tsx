e95e1c0b279ee3b5c333d7b0091ce4d6e59b512a
panan <3234059020@qq.com>
2022-09-11 00:26:42 +0800

    second commit

--- src/pages/Access/index.tsx
+++ src/pages/Access/index.tsx
@@ -12,7 +12,7 @@ const AccessPage: React.FC = () => {
       }}
     >
       <Access accessible={access.canSeeAdmin}>
-        <Button type='primary'>只有 Admin 可以看到这个按钮</Button>
+        <Button>只有 Admin 可以看到这个按钮</Button>
       </Access>
     </PageContainer>
   );
